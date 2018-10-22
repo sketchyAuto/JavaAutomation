fca2c7a560feb92639bfaf6602330cdcaba5c942
sketchyAuto <41617316+sketchyAuto@users.noreply.github.com>
2018-10-22 16:32:13 +0100

    Add files via upload

---
+++ ExamsDetails.java
@@ -0,0 +1,12 @@
+package LeanAndHomeExams;
+
+public class ExamsDetails {
+
+    public static void main(String[] args){
+        //Object of the StudentResults class
+        StudentResults aStudents = new StudentResults();
+
+
+    }
+
+}
---
+++ StudentResults.java
@@ -0,0 +1,14 @@
+package LeanAndHomeExams;
+
+public class StudentResults {
+    //Note: The variables you set up inside of methods are only available to those methods.
+    // They are not available to other methods. They are said to have local scope
+    //These four fields will be available to all the methods that we write in this class,
+    // and won't be local to any one method. They are said to have global scope.
+
+    String full_Name;
+    String exam_Name;
+    String exam_Score;
+    String exam_Grade;
+
+}
